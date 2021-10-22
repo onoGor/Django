2312134339a557fbcf29f0f53f4025a1e5eabad3
onoGor <faraoh5552@yandex.ru>
2021-10-22 19:44:25 +0300

    add lesson_5

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from mainapp.models import ProductCategory
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Каталог'
+    links_menu = ProductCategory.objects.all()
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    return render(request, 'mainapp/products.html', context)
